235ea30a5c262e4705fa0cdde81f5b0ac3ed0da0
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-07-23 11:02:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -660,9 +660,11 @@ def generate_enemy():
 
 def generate_problem():
     rand = random.random()
-    if rand < 0.20:  # 25% math problems
+    if rand < 0.25:  # 25% math problems
         return generate_math_problem()
-    elif rand < 0.15:  # 20% rock paper scissors
+    elif rand < 0.50:  # 15% numerical questions
+        return generate_numerical_question()
+    elif rand < 0.75:  # 20% rock paper scissors
         return generate_rps_problem()
     else:  # 40% multiple choice questions
         return generate_multiple_choice_question()
